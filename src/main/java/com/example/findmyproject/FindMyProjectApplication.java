e7155bdc1f5e5390279d2aa3786de31c541b2e22
lakshmi0398 <lakshmiaviligonda30@gmail.com>
2024-03-03 05:16:53 +0000

    Message

---
+++ src/main/java/com/example/findmyproject/FindMyProjectApplication.java
@@ -0,0 +1,11 @@
+package com.example.findmyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMyProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FindMyProjectApplication.class, args);
+    }
+}
